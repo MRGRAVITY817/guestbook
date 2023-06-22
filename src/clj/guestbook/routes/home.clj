10c103d28780119b0870f6b1983bc6994b5ab0a7
Hoon Wee <mrgravity817@gmail.com>
2023-06-22 10:39:35 +0900

    feat: add form validation and flash error messages

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -4,20 +4,40 @@
    [guestbook.db.core :as db]
    [clojure.java.io :as io]
    [guestbook.middleware :as middleware]
+   [struct.core :as st]
    [ring.util.response]
    [ring.util.http-response :as response]))
 
-(defn home-page [request]
+(defn home-page [{:keys [flash] :as request}]
   (layout/render
-   request "home.html"
-   {:messages (db/get-messages)}))
+   request
+   "home.html"
+   (merge
+    {:messages (db/get-messages)}
+    (select-keys flash [:name :message :errors]))))
 
 (defn about-page [request]
   (layout/render request "about.html"))
 
+;; form validation using `Struct` library (it's like zod in Node.js)
+(def message-schema [[:name st/required st/string]
+                     [:message st/required st/string
+                      {:message "message must contain at least 10 characters"
+                       :validate #(>= (count %) 10)}]])
+
+(defn validate-message [params]
+  ;; first element of st/validate returns nil when data is valid
+  (first (st/validate params message-schema)))
+
 (defn save-message! [{:keys [params]}]
-  (db/save-message! params)
-  (response/found "/"))
+  (if-let [errors (validate-message params)]
+    ;; If form input is invalid, show error message via flash.
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    ;; Else, save the message in database.
+    (do
+      (db/save-message! params)
+      (response/found "/"))))
 
 (defn home-routes []
   [""
