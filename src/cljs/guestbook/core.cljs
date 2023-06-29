29e98d479c4e930b4611d3abb5d1ee65c4acd3bd
Hoon Wee <mrgravity817@gmail.com>
2023-06-29 15:18:01 +0900

    feat: use server state with reframe events

--- src/cljs/guestbook/core.cljs
+++ src/cljs/guestbook/core.cljs
@@ -7,10 +7,68 @@
    [clojure.string :as string]
    [guestbook.validation :refer [validate-message]]))
 
-(defn get-messages [messages]
+;; Create Reframe event, which contains info about 
+;; whether messages are being loaded or not.
+(rf/reg-event-fx
+ :app/initialize
+ (fn [_ _]
+   {:db {:messages/loading? true}}))
+
+;; Create subscription so that other function components can
+;; subscribe to :messages/loading? event.
+(rf/reg-sub
+ :messages/loading?
+ (fn [db _]
+   (:messages/loading? db)))
+
+;; Use reg-event-db for events that only use db effect.
+(rf/reg-event-db
+ :messages/set
+ (fn [db [_ messages]]
+   (-> db (assoc :messages/loading? false
+                 :messages/list messages))))
+
+;; Get messages
+
+(rf/reg-sub
+ :messages/list
+ (fn [db _]
+   (:messages/list db [])))
+
+(defn get-messages []
   (GET "/messages"
     {:headers {"Accept" "application/transit+json"}
-     :handler #(reset! messages (:messages %))}))
+     :handler #(rf/dispatch [:messages/set (:messages %)])}))
+
+;; Send message event
+(rf/reg-event-db
+ :messages/add
+ (fn [db [_ message]]
+   ;; Return db map where :messages/list has been 
+   ;; appended with new message at front (conjoined) .
+   (update db :messages/list conj message)))
+
+;; Actual function to send message
+(defn send-message!
+  "Send form values to server (name and message)"
+  [fields errors]
+  (if-let [validation-errors (validate-message @fields)]
+    (reset! errors validation-errors)
+    (POST "/message"
+      {:format :json
+       :headers {;; Transit will auto convert Clojure data structures 
+                 ;; into HTML friendly string
+                 "Accept" "application/transit+json"
+                 "x-csrf-token" (.-value (.getElementById js/document "token"))}
+       :params @fields
+       :handler (fn [_]
+                  (rf/dispatch
+                   [:messages/add (assoc @fields :timestamp (js/Date.))])
+                  (reset! fields nil)
+                  (reset! errors nil))
+       :error-handler (fn [e]
+                        (.log js/console (str e))
+                        (reset! errors (-> e :response :errors)))})))
 
 (defn message-list [messages]
   (println messages)
@@ -22,33 +80,11 @@
       [:p message]
       [:p " - " name]])])
 
-(defn send-message!
-  "Send form values to server (name and message)"
-  [fields errors messages]
-  (if-let [validation-errors (validate-message @fields)]
-    (reset! errors validation-errors)
-    (POST "/message"
-      {:format :json
-       :headers {;; Transit will auto convert Clojure data structures 
-               ;; into HTML friendly string
-                 "Accept" "application/transit+json"
-                 "x-csrf-token" (.-value (.getElementById js/document "token"))}
-       :params @fields
-       :handler       (fn [_]
-                        (swap! messages conj
-                               (assoc @fields
-                                      :timestamp (js/Date.)))
-                        (reset! fields nil)
-                        (reset! errors nil))
-       :error-handler (fn [e]
-                        (.log js/console (str e))
-                        (reset! errors (-> e :response :errors)))})))
-
 (defn errors-component [errors id]
   (when-let [error (id @errors)]
     [:div.notification.is-danger (string/join error)]))
 
-(defn message-form [messages]
+(defn message-form []
   (let [fields (r/atom {})
         errors (r/atom nil)]
     (fn []
@@ -76,36 +112,23 @@
        ;; Submit Button
        [:input.button.is-primary
         {:type :submit
-         :on-click #(send-message! fields errors messages)
+         :on-click #(send-message! fields errors)
          :value "comment"}]])))
 
-;; Create Reframe event, which contains info about 
-;; whether messages are being loaded or not.
-(rf/reg-event-fx
- :app/initialize
- (fn [_ _]
-   {:db {:messages/loading? true}}))
-
-;; Create subscription so that other function components can
-;; subscribe to :messages/loading? event.
-(rf/reg-sub
- :messages/loading?
- (fn [db _]
-   (:messages/loading? db)))
-
 (defn home []
-  (let [messages (r/atom nil)]
-    (rf/dispatch [:app/initialize])
-    (get-messages messages)
+  (let [messages (rf/subscribe [:messages/list])] ;; Local state
+    (rf/dispatch [:app/initialize]) ;; Put event in queue
+    (get-messages)
     (fn []
-      (if @(rf/subscribe [:messages/loading?])
-        [:div>div.row>div.span12>h3 "Loading Messages..."]
-        [:div.content>div.columns.is-centered>div.column.is-two-thirds
-         [:div.columns>div.column
-          [:h3 "Messages"]
-          [message-list messages]]
-         [:div.columns>div.column
-          [message-form messages]]]))))
+      [:div.content>div.columns.is-centered>div.column.is-two-thirds
+       (if @(rf/subscribe [:messages/loading?])
+         [:h3 "Loading Messages..."]
+         [:div
+          [:div.columns>div.column
+           [:h3 "Messages"]
+           [message-list messages]]
+          [:div.columns>div.column
+           [message-form]]])])))
 
 (dom/render
  [home]
