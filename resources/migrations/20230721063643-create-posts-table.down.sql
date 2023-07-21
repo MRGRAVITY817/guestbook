1ddfc207576fa13dd0487a34b58aff867bf06b89
Hoon Wee <mrgravity817@gmail.com>
2023-07-21 16:00:51 +0900

    feat: update query api

--- resources/migrations/20230721063643-create-posts-table.down.sql
+++ resources/migrations/20230721063643-create-posts-table.down.sql
@@ -1,5 +1,1 @@
-CREATE TABLE posts
-(id SERIAL PRIMARY KEY,
- name TEXT not null,
- message TEXT not null,
- timestamp TIMESTAMP not null DEFAULT now());
+DROP TABLE posts;
--- resources/migrations/20230721063643-create-posts-table.up.sql
+++ resources/migrations/20230721063643-create-posts-table.up.sql
@@ -1,1 +1,5 @@
-DROP TABLE posts;
+CREATE TABLE posts
+(id SERIAL PRIMARY KEY,
+ name text not null,
+ message text not null,
+ timestamp TIMESTAMP not null DEFAULT now());
